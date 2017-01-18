f5c59890dfcf54710e385ab9aed45db633114bce
agutierrez58 <agutierrez@jaumebalmes.net>
2017-01-18 20:00:56 +0100

    Pujada inicial

---
+++ ConwayGameTDD/src/org/tdd/Board.java
@@ -0,0 +1,17 @@
+package org.tdd;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Board {
+
+	public List<Cell> getNeighbors(Cell cell) {		
+		return Collections.emptyList();
+	}
+
+	public void addCell(Cell cell) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ConwayGameTDD/src/org/tdd/GoGrid.java
@@ -0,0 +1,27 @@
+package org.tdd;
+
+public class GoGrid {
+	/**
+	 * 
+	 * Based on TDD Kata 
+	 * https://gojko.net/2009/02/27/thought-provoking-tdd-exercise-at-the-software-craftsmanship-conference/
+	 * 
+			Attendees were split into pairs who were working on the task together, using TDD practices with very strict rules:
+			
+			1) write exactly ONE failing test
+			2) make the test from (1) pass by first writing implementation code IN THE TEST
+			3) create a new implementation method/function by:
+			     doing extract method on implementation code created as per (2), or
+			     moving implementation code as per (2) into an existing implementation method
+			4) only ever create new methods IN THE TEST CLASS
+			5) only ever create implementation classes to provide a destination for extracting a method created as per (4).
+			6)populate implementation classes by doing move method from a test class into them
+			7) refactor as required
+			8) go to (1)
+
+	 * @author agutierrez
+	 *
+	 */
+
+
+}
